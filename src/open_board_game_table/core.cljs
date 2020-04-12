6cb45323379e7dcea438c7d9765b43cff991c106
Kamil Stasiak <kamilxstasiak@gmail.com>
2020-04-12 19:53:46 +0200

    Working moving

--- src/open_board_game_table/core.cljs
+++ src/open_board_game_table/core.cljs
@@ -5,6 +5,14 @@
 
 (println "-> Reloaded")
 
+(defn drop-nth [n coll]
+  (keep-indexed #(if (not= %1 n) %2) coll))
+
+(defn add-nth [n element coll]
+  (concat (subvec coll 0 n) [element] (subvec coll n)))
+
+(add-nth 0 "x" [{:a "aaa"}])
+
 (def element
   {:active :back
    :selected? false
@@ -19,8 +27,8 @@
    (js/console.log "Initialize fired!")
    {:selected nil
     :message "init"
-    :decks {:player1 [element element element]
-            :player2 [element]}}))
+    :decks {:player1 (list element element element)
+            :player2 (list element)}}))
 
 (r/reg-sub
  :query-selected
@@ -29,7 +37,15 @@
 
 (defn action-component [selected]
   (println selected)
-  [:div "hello world" (str selected)])
+  (when selected
+    [:div
+     [:div "selected: " (str selected)]
+     [:h4
+      {:on-click #(r/dispatch [:move-card selected :player1])}
+      "Move to player1"]
+     [:h4
+      {:on-click #(r/dispatch [:move-card selected :player2])}
+      "Move to player2"]]))
 
 (r/reg-sub
  :query-deck
@@ -51,12 +67,30 @@
 (r/reg-event-fx
  :select-card
  (fn [{:keys [db]} [_ deck idx]]
-   (let [card (get-in db [:decks deck idx])
+   (let [deck1 (get-in db [:decks deck])
+         card (nth deck1 idx)
          toggled-card (assoc card :selected? (not (:selected? card)))
          new-db (assoc db :selected [deck idx])
-         new-db2 (assoc-in new-db [:decks deck idx] toggled-card)]
-     {:db new-db2})
-   ))
+         new-deck (assoc (vec deck1) idx toggled-card)
+         new-db2 (assoc-in new-db [:decks deck] new-deck)
+         ]
+     ; (println "--->>> " new-db2)
+     {:db new-db2})))
+
+(r/reg-event-fx
+ :move-card
+ (fn [{:keys [db]} [_ [player idx] to]]
+   (println (str "Move from " [player idx] " to " to))
+   (let [
+         from-deck (get-in db [:decks player])
+         element (nth from-deck idx)
+         new-from-deck (drop-nth idx from-deck)
+         to-deck (get-in db [:decks to])
+         new-to-deck (add-nth 0 element (vec to-deck))
+         db1 (assoc-in db [:decks player] new-from-deck)
+         db2 (assoc-in db1 [:decks to] new-to-deck)
+         ]
+     (println db2) {:db db2})))
 
 (defn card-component [{:keys [active selected?] :as e} on-click]
   (let [image (:img (active e))]
@@ -74,21 +108,22 @@
     {:style {:width "500px" :height "200px" :display "flex" :justify-content "space-around"}}
     (for [[index element] (map-indexed vector cards)]
       ^{:key index} [card-component element
-                     #(r/dispatch [:select-card deck-name index])]
-      )]])
+                     (fn []
+                       (js/console.log (str "Card clicked!" index) )
+                       (r/dispatch [:select-card deck-name index]))])]])
 
 (defn app []
   [:div.my-app {:on-click #(js/console.log "background clicked")}
    (when-let [deck @(r/subscribe [:query-deck :player1])]
-    [deck-component "First player" deck :player1])
+     [deck-component "First player" deck :player1])
    (when-let [deck @(r/subscribe [:query-deck :player2])]
      [deck-component "Second player" deck :player2])
    (when-let [first-card (first @(r/subscribe [:query-deck]))]
      [card-component first-card])
-   [action-component @(r/subscribe [:query-selected])]
-   ])
+   [action-component @(r/subscribe [:query-selected])]])
 
 ;; rendering
+
 
 (defn render []
   (d/render [app] (js/document.getElementById "app")))
